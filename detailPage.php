064f3c02c53a0cf8b4655b87eb1eccd3a5f40f20
张智杰 <uestczzj@163.com>
2019-04-20 21:51:34 +0800

    use node js to decode

--- detailPage.php
+++ detailPage.php
@@ -27,16 +27,27 @@ function singlePage($page_url, $title)
 
 	$header = "Accept-Language:zh-CN,zh;q=0.9\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)Chrome/51.0.2704.106 Safari/537.36\r\nX-Forwarded-For:".random_ip()."\r\nreferer:".$page_url."\r\nContent-Type: multipart/form-data; session_language=cn_CN";
 	$page = new Document($page_url, true, $header);
-	$date = $page->find('//*[@id="videodetails-content"]/span[2]', Query::TYPE_XPATH)[0]->text();
-
-	$shareLink = $page->first('#linkForm2')->first('#fm-video_link')->text();
-	$sharePage = new Document(file_get_contents($shareLink), false, $header);
-
-	$videoUrl = $sharePage->first('source')->getAttribute('src');
+	$cipher = $page->first('#vid')->first('script')->text();
+	$videoUrl = decode($cipher);
     echo $videoUrl."\n";
    	
    	download($videoUrl, $title);
 
+}
+
+function decode($cipher)
+{
+	$js = file_get_contents('http://91porn.com/js/md5.js');
+	$file = fopen('./md5.js',"w+");
+	fputs($file,$js.'console.log(strencode(process.argv[2], process.argv[3], process.argv[4]));');//写入文件
+	fclose($file);
+	$cipher = substr($cipher, 55);
+	$cipher = substr($cipher, 0, strlen($cipher)-19);
+	$cipher = str_replace('","', ' ', $cipher);
+	$tag = shell_exec('node ./md5.js '.$cipher);
+	$videoUrl = explode("<source src='", $tag)[1];
+	$videoUrl = explode("' type='video/mp4", $videoUrl)[0];
+	return $videoUrl;
 }
 
 function download($videoUrl, $title) {
