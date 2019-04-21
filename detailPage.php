a84c3c636f0b5fb0a079461cfff555a2ee64310c
张智杰 <uestczzj@163.com>
2019-04-21 23:00:01 +0800

    improve the stability of the downloading

--- detailPage.php
+++ detailPage.php
@@ -15,15 +15,19 @@ function random_ip()
 
 function singlePage($page_url, $title)
 {
-	$defaultPath = './videos';
-	if (!is_dir($defaultPath)) {
-		mkdir($defaultPath);
-	}
-	$filePath = $defaultPath.'/'.$title.'.mp4';
-	if (file_exists($filePath)){
-		echo "\033[0;34m"."文件已存在"."\033[0m\n";
+	// $aria2 = new Aria2('http://127.0.0.1:6800/jsonrpc');
+	// $result = $aria2->getGlobalStat();
+	// if ($result['result']['numWaiting'] >= 10) {	//提交到aria2的任务数过多后容易下载失败，此处限定最多等待任务为10个
+	// 	echo "等待。。。\n";
+	// 	sleep(5);
+	// 	singlePage($page_url, $title);
+	// 	return;
+	// }
+
+	if (checkExist($title)) {
 		return;
 	}
+	
 
 	$header = "Accept-Language:zh-CN,zh;q=0.9\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)Chrome/51.0.2704.106 Safari/537.36\r\nX-Forwarded-For:".random_ip()."\r\nreferer:".$page_url."\r\nContent-Type: multipart/form-data; session_language=cn_CN";
 	$page = new Document($page_url, true, $header);
@@ -61,6 +65,39 @@ function download($videoUrl, $title) {
 	}else {
 		echo "\033[0;31m"."提交Aria2失败！！！"."\033[0m\n";
 	}
+}
+
+function checkExist($title) {
+	$filePath = __DIR__.'/videos/'.$title.'.mp4';
+
+	if (file_exists($filePath)){
+		echo "\033[0;34m"."文件已存在"."\033[0m\n";
+		return true;
+	}
+
+	$aria2 = new Aria2('http://127.0.0.1:6800/jsonrpc');
+
+	foreach ($aria2->tellActive()['result'] as $item) {
+		if ($item['files'][0]['path'] == $filePath) {
+			echo "\033[0;34m"."该视频正在下载"."\033[0m\n";
+			return true;
+		}
+	}
+
+	foreach ($aria2->tellWaiting(0, 1000)['result'] as $item) {
+		if ($item['files'][0]['path'] == $filePath) {
+			echo "\033[0;34m"."该视频正在等待下载"."\033[0m\n";
+			return true;
+		}
+	}
+
+	foreach ($aria2->tellStopped(0, 1000)['result'] as $item) {
+		if ($item['files'][0]['path'] == $filePath) {
+			echo "\033[0;34m"."该视频已经下载"."\033[0m\n";
+			return true;
+		}
+	}
+	return false;
 }
 
 if (count($argv)>1) {
